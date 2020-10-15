cf233866f1aefcc2c8864d32fa555840dc7e53fd
benvenutim93 <56692322+benvenutim93@users.noreply.github.com>
2020-10-15 10:56:06 -0300

    Validaciones

--- TPFinal/Views/modify-form-cinema.php
+++ TPFinal/Views/modify-form-cinema.php
@@ -5,21 +5,21 @@
         <h1 class="h3 mb-3 font-weight-normal">Cine</h1>
             <!--Sacando el sr-only te muestra el titulo(LABEL)-->
             <!-- L A B E L -->
-            <label for="name" class="sr-only">Ingrese Nuevo Nombre </label>
+            <label for="name" class="">Ingrese Nuevo Nombre </label>
             <!--I N P U T-->
-            <input type="text" id="name" class="form-control" value="<?php echo $movie->getName()?>" name="name" required autofocus>
+            <input type="text" id="name" class="form-control" value="<?php echo $movie->getName()?>" name="name" required autofocus required minlength="3">
             <!-- L A B E L -->
-            <label for="address" class="sr-only">Ingrese Nueva Direccion</label>
+            <label for="address" class="">Ingrese Nueva Direccion</label>
             <!--I N P U T-->
-            <input type="text" id="address" class="form-control" value="<?php echo $movie->getAddress()?>"  name="address" required>
+            <input type="text" id="address" class="form-control" value="<?php echo $movie->getAddress()?>"  name="address" required  minlength="5" required>
             <!-- L A B E L -->
-            <label for="capacity" class="sr-only">Ingrese Nueva Capacidad</label>
+            <label for="capacity" class="">Ingrese Nueva Capacidad</label>
             <!--I N P U T-->
-            <input type="text" id="capacity" class="form-control" value="<?php echo $movie->getCapacity()?>"  name="capacity" required>
+            <input type="number"  id="capacity" class="form-control" value="<?php echo $movie->getCapacity()?>"  name="capacity" min="0" max="500" required>
             <!-- L A B E L -->
-            <label for="ticketValue" class="sr-only">Ingrese Nuevo Valor de Entrada</label>
+            <label for="ticketValue" class="">Ingrese Nuevo Valor de Entrada</label>
             <!--I N P U T-->
-            <input type="text" id="ticketValue" class="form-control" value="<?php echo $movie->getTicketValue()?>"  name="ticketValue" required>
+            <input type="number" id="ticketValue" class="form-control" value="<?php echo $movie->getTicketValue()?>"  name="ticketValue" min="0" max="500" required>
 
         <!-- B O T O N -->
              <button class="btn btn-lg btn-primary btn-block" type="submit">Modificar</button>
